2ebd676c46c6bca0e2623094e0fcca575656bf23
Hernande Monteiro <monteiro1998@live.com>
2022-09-11 13:08:17 -0300

    fix code smell docs

--- src/providers/useAuth.tsx
+++ src/providers/useAuth.tsx
@@ -1,32 +1,26 @@
 import { createContext, useContext, useState } from "react";
 
-/* @description provide an global state for user login status;
- * @param {function} setUser, used by change login status;
+/* @description provide an global state ccontext for user login status;
+ * @Hook [useState] used by change login status with "setUser";
  * @return an object with user login informations;
  * -----------------------------------------------
  */
 
-// interface types to TS;
 interface AuthProps {
   children?: any;
 }
 
-// create the global context;
 export const AuthContext = createContext<any>({});
 
-// create the global context provider;
 export const AuthProvider = (props: AuthProps) => {
-  // define an initial State for the user login;
   const initialState = {
     UserId: "",
     Name: "",
     Type: "",
     isLogged: false,
   };
-  // useState hook to change the user login status;
   const [user, setUser] = useState<any>(initialState);
 
-  // send the infos to a global context;
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       {props.children}
@@ -34,5 +28,4 @@ export const AuthProvider = (props: AuthProps) => {
   );
 };
 
-// export the global context in hook format;
 export const useAuth = () => useContext(AuthContext);
