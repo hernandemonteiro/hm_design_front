ed5a6318c1d6c179d1887b6f6c19ae0c5153b1e3
Hernande Monteiro <monteiro1998@live.com>
2022-09-18 19:20:25 -0300

    recovery password page created

--- src/components/System/RecoveryPassword/index.tsx
+++ src/components/System/RecoveryPassword/index.tsx
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Button from "../../UI/Button";
+import "./RecoveryPassword.scss";
+import forgotFailure from "../../../assets/images/forgotsuccess.svg";
+import { useParams } from "react-router-dom";
 
-export default function RecoveryPassword(){
-    return (
-        <h1>Recovery Password</h1>
-    )
+export default function RecoveryPassword() {
+  const [message, setMessage] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [view, setView] = useState(true);
+  const hash = useParams().hash;
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/confirmHash/${hash}`)
+      .then((response: any) => response.json())
+      .then((response: any) => {
+        if (response.result === 0) {
+          setView(false);
+        }
+      });
+  }, []);
+  function recoveryPassword(event: any) {
+    event.preventDefault();
+    fetch(`${import.meta.env.VITE_API_URL}/updatePassword/${hash}/${password}`)
+      .then((response: any) => {
+        if (response.result === "Success") {
+          window.location.href = "/login";
+        }
+        setMessage("Erro ao mudar senha!");
+      })
+      .catch((error: any) => {
+        setMessage("Erro ao mudar senha!");
+        console.log(error);
+      });
+  }
+  return (
+    <div className="RecoveryBox">
+      {view ? (
+        <form onSubmit={recoveryPassword}>
+          <p>Digite sua nova senha!</p>
+          <h3 className="MessageSystem">{message}</h3>
+          <br />
+          <input
+            type="password"
+            required
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Nova Senha"
+          />
+          <input
+            type="password"
+            required
+            className={password != confirmPassword ? "outlineRed" : ""}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+            placeholder="Confirmar Nova Senha"
+          />
+          {password != "" &&
+          password === confirmPassword &&
+          confirmPassword != "" ? (
+            <Button type={"submit"} className="green">
+              RECUPERAR
+            </Button>
+          ) : (
+            "Senha e confirmação devem ser iguais!"
+          )}
+
+          <Button
+            type={"button"}
+            onClick={() => (window.location.href = "/login")}
+          >
+            IR AO LOGIN
+          </Button>
+        </form>
+      ) : (
+        <div className="forgotFailure">
+          <img src={forgotFailure} />
+          <br />
+          <h1>Não foi encontrada solicitação para esse email!</h1>
+          <br />
+          <Button onClick={() => (window.location.href = "/forgotpassword")}>
+            Tentar Novamente!
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
