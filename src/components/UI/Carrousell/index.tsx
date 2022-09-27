d493ca35656a63de0e82e3c1a25d81a394f10cfe
Hernande Monteiro <monteiro1998@live.com>
2022-09-26 21:24:16 -0300

    modify carroussel method

--- src/components/UI/Carrousell/index.tsx
+++ src/components/UI/Carrousell/index.tsx
@@ -7,48 +7,34 @@ interface carrousselProps {
 }
 
 export default function Carroussel(props: carrousselProps) {
-  const [carrousselPrevious, setCarrousselPrevious] = useState(0);
-  const [carrousselNext, setCarrousselNext] = useState(1);
+  const [carroussel, setCarroussel] = useState(0);
   const images = props.data;
-  console.log("Previous: " + carrousselPrevious, "Next :" + carrousselNext);
   return (
     <div className="carroussel">
       {images
         ? JSON.parse(images).length > 0 &&
-          carrousselNext > 1 && (
+          carroussel > 0 && (
             <SkipPreviousOutlined
-              onClick={() => {
-                setCarrousselPrevious(carrousselPrevious - 1);
-                setCarrousselNext(carrousselNext - 1);
-              }}
+              onClick={() => setCarroussel(carroussel - 1)}
             />
           )
         : ""}
-      {images
-        ? JSON.parse(images)
-            .slice(carrousselPrevious, carrousselNext)
-            .map((element: any) => (
-              <img
-                className="image"
-                src={`https://drive.google.com/uc?export=view&id=${element.id}`}
-              />
-            ))
-        : ""}
       {images ? (
-        JSON.parse(images).length > 0 &&
-        carrousselNext < JSON.parse(images).length ? (
-          <SkipNextOutlined
-            onClick={() => {
-              setCarrousselPrevious(carrousselNext);
-              setCarrousselNext(carrousselNext + 1);
-            }}
-          />
-        ) : (
-          ""
-        )
+        <img
+          className="image"
+          src={`https://drive.google.com/uc?export=view&id=${
+            JSON.parse(images)[carroussel].id
+          }`}
+        />
       ) : (
         ""
       )}
+      {images
+        ? JSON.parse(images).length > 0 &&
+          carroussel < JSON.parse(images).length - 1 && (
+            <SkipNextOutlined onClick={() => setCarroussel(carroussel + 1)} />
+          )
+        : ""}
     </div>
   );
 }
