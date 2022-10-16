52e1331d54cc2836e1d0129b8c6f8399a4a15b09
Hernande Monteiro <monteiro1998@live.com>
2022-10-16 00:05:18 -0300

    changes in main imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import Loader from "./components/UI/Loader";
 import Main from "./components/UI/Main";
 import Router from "./router";
 
